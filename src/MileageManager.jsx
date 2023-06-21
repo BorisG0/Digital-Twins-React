9ec10acd6f02482d2826ef18e369fc20c37dad2a
Boris Gratchev <boris.gratchev@sap.com>
2023-06-21 13:02:29 -0700

    minting new bike

--- src/MileageManager.jsx
+++ src/MileageManager.jsx
@@ -4,28 +4,36 @@ import React, { useEffect, useState } from 'react';
 import { ethers } from "ethers";
 
 export function MileageManager(props){
+    const abi = require("./abi/BikeTwins.json");
 
     const [tokenId, setTokenId] = useState("");
     const [amount, setAmount] = useState("");
 
-    async function mintNFT() {
-        console.log("Calling Contract");
-        console.log(props.address);
-        console.log(tokenId);
+    const [receiver, setReceiver] = useState("");
 
-        const abi = require("./abi/BikeTwins.json");
-
+    async function increaseMileage() {
         try{
-            const accounts = await window.ethereum.request({
-                method: "eth_requestAccounts",
-            });
-
+            await window.ethereum.send("eth_requestAccounts");
             const provider = new ethers.providers.Web3Provider(window.ethereum);
             const signer = provider.getSigner();
 
             const contract = new ethers.Contract(props.address, abi, signer);
 
             await contract.increaseMileage(tokenId, amount);
+        }catch(err){
+            console.log(err);
+        }
+    }
+    
+    async function mintBike() {
+        try{
+            await window.ethereum.send("eth_requestAccounts");
+            const provider = new ethers.providers.Web3Provider(window.ethereum);
+            const signer = provider.getSigner();
+
+            const contract = new ethers.Contract(props.address, abi, signer);
+
+            await contract.safeMint(receiver, tokenId);
         }catch(err){
             console.log(err);
         }
@@ -39,7 +47,13 @@ export function MileageManager(props){
             <TextField type="text" placeholder="Amount" value={amount} onChange={e => setAmount(e.target.value)}/>
 
             <br/>
-            <Button variant="contained" onClick={mintNFT}>Mint</Button>
+            <Button variant="contained" onClick={increaseMileage}>Increase</Button>
+
+            <h1>Mint new Bike</h1>
+            <TextField type="text" id="receiver" placeholder="Receiver" value={receiver} onChange={e => setReceiver(e.target.value)}/>
+            <TextField type="number" id="tokenId" placeholder="Token ID" value={tokenId} onChange={e => setTokenId(e.target.value)}/>
+            <br/>
+            <Button variant="contained" onClick={mintBike}>Mint</Button>
         </>
         
     )
